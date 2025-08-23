42e2b62518979077cf9cc48b0f53faac50171853
etorth <anhonghe@gmail.com>
2025-08-23 11:25:15 -0400

    f

--- client/src/gfxcropboard.hpp
+++ client/src/gfxcropboard.hpp
@@ -89,10 +89,6 @@ class GfxCropBoard: public Widget
                 return;
             }
 
-            if(!gfxWidget()){
-                return;
-            }
-
             const int brdCropXOrig = gfxCropX();
             const int brdCropYOrig = gfxCropY();
 
